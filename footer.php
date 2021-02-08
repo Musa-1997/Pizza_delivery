1f0be29e7d5391325ed59a1cca17740b371e39f9
Musa-1997 <69716703+Musa-1997@users.noreply.github.com>
2021-02-08 14:23:02 -0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+
+
+<div class="footer" style="background-color:#0a043c; height:45px; border-radius:10px; ">
+
+   <a href="index.php"><h1  style="font-size:20px; color:#03506f;"class="text-center pb-1" > <i  style="background-color:yellow ;font-size:40px; border-radius:20px;" class="far fa-grin-alt "></i> <span style="color:#f8f1f1">Developed</span> <span style="color:#ec4646">by</span> <span style="color:#ffe227">Mussa</span> </h1></a>
+</div>
+
+<script script src="js/jquery-3.2.1.slim.min.js"></script>
+<script script src="js/bootstrap.min.js"></script>
+<script script src="js/popper.min.js"></script>
